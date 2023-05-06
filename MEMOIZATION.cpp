ec2c625488c5169abcb1591048b7a55ca2a25243
Shreyansh yadav <107577979+yadavshreyansh@users.noreply.github.com>
2023-05-06 22:41:15 +0530

    Create MEMOIZATION.cpp

---
+++ MEMOIZATION.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    bool solve(int N, int K, int target, vector<int> &coins, vector<vector<int>> &dp) {
+            
+        if (K == 0 and target == 0) return true;
+        
+        if (dp[K][target] != -1) return dp[K][target];
+        bool ans = false;
+        for (int i = 0; i < N; i++) {
+            if(target>=coins[i] and K-1>=0)
+                ans |= solve(N, K - 1, target - coins[i], coins, dp);
+        }
+        
+        return dp[K][target] = ans;
+        
+    }
+    bool makeChanges(int N, int K, int target, vector<int> &coins) {
+        vector<vector<int>> dp(K+1, vector<int>(target+1, -1));
+        return solve(N, K, target, coins, dp);
+    }
+};
