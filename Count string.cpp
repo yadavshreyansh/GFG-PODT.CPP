28637d9e0d79f27cb8bfe9ce4f9cf533ee453890
Shreyansh yadav <107577979+yadavshreyansh@users.noreply.github.com>
2023-05-09 23:56:40 +0530

    Create Count string.cpp

---
+++ Count string.cpp
@@ -0,0 +1,11 @@
+int ans=0,temp=1;
+		for(int i=s.length()-1;i>=0;i--){
+		    if(s.charAt(i)=='1'){
+		        ans+=temp;
+		        ans%=m;
+		    }
+		    temp*=2;
+		    temp%=m;
+		}
+		return ans;
+    }
