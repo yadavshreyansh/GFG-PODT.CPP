2ee668cca1d462765168f5f52df15dc426a6e1a8
Shreyansh yadav <107577979+yadavshreyansh@users.noreply.github.com>
2023-07-01 18:25:01 +0530

    Create Number of 1 Bits.cpp

---
+++ Number of 1 Bits.cpp
@@ -0,0 +1,14 @@
+
+class Solution {
+  public:
+    int setBits(int N) {
+        int ans = 0;
+        
+        while(N){
+            ++ans;
+            N &= (N - 1);
+        }
+        
+        return ans;
+    }
+};
