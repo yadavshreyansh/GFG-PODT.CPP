2a61e60a6608a797de64ecf527aa820c5fbd0126
Shreyansh yadav <107577979+yadavshreyansh@users.noreply.github.com>
2023-07-06 18:45:58 +0530

    Create Quick Sort .cpp

---
+++ Quick Sort .cpp
@@ -0,0 +1,42 @@
+
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to sort an array using quick sort algorithm.
+    void quickSort(int arr[], int low, int high)
+    {
+        if(low < high){
+            int p = partition(arr, low, high);
+            
+            quickSort(arr, low, p - 1);
+            quickSort(arr, p + 1, high);
+        }
+    }
+    
+    public:
+    int partition (int arr[], int low, int high)
+    {
+       int p = low;
+       int pivot = arr[high];
+       
+       for(int i = low; i < high; i++){
+           if(arr[i] < pivot){
+               swap(arr[i], arr[p]);
+               ++p;
+           }
+       }
+       
+       swap(arr[p], arr[high]);
+       
+       return p;
+    }
+};
