33efafc60d3bff49c375de6631da7b986808d71f
Shreyansh yadav <107577979+yadavshreyansh@users.noreply.github.com>
2023-07-16 22:17:57 +0530

    Create Queue Reversal.cpp

---
+++ Queue Reversal.cpp
@@ -0,0 +1,21 @@
+
+class Solution
+{
+    public:
+    void helper(queue<int> &q){
+        if(q.size() == 0)
+            return;
+            
+        int x = q.front();
+        q.pop();
+        helper(q);
+        q.push(x);
+    }
+    
+    queue<int> rev(queue<int> q)
+    {   
+        helper(q);
+        
+        return q;
+    }
+};
